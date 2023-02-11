d4e0ee446e9b331341aaf859a8714188fa49058f
LucasHVicente <l.vicente9003@gmail.com>
2023-02-11 00:16:30 -0300

    add: implementation of error message component

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-if="errorMessage" class="error-container">
+    <h3>Error</h3>
+    <p>{{ errorMessage }}</p>
+  </div>
+</template>
+<style scoped src="../styles/components/ErrorMessage.scss" />
+<script setup>
+import { computed } from "vue";
+import { usePokemon } from "../stores/pokemon";
+
+const pokemonStore = usePokemon();
+const errorMessage = computed(() => pokemonStore.getErrorMessage);
+</script>
